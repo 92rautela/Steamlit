c09bc0c82a0e539fa466f579b172a694f305c00b
92rautela <92rautela@gmail.com>
2025-09-05 00:59:33 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from datetime import date
+
+st.title("🧮 Age Calculator")
+
+min_dob = date(1900, 1, 1)
+max_dob = date.today()
+
+# Take DOB input
+dob = st.date_input("Enter your Date of Birth",min_value=min_dob)
+
+# Get today's date
+today = date.today()
+
+# Calculate age
+age_years = today.year - dob.year
+
+# Show result
+st.write(f"🎂 Your age is: **{age_years}** years")
